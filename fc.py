41a57b09a96b4043aaece38838947fc3627d8825
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2012-07-27 23:54:38 +0900

    add schedule page

--- fc.py
+++ fc.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'foo_bar-baz'
 
 import sqlite3
+import json
 from contextlib import closing
 
 
@@ -19,7 +20,7 @@ DATABASE_URI = 'fc.db'
 
 
 def connect_db():
-    db = sqlite3.connect(DATABASE_URI)
+    db = sqlite3.connect(DATABASE_URI, detect_types=sqlite3.PARSE_DECLTYPES)
     db.row_factory = sqlite3.Row
     return db
 
@@ -35,10 +36,24 @@ def init_db():
 # DOMAIN LEVEL DATABASE ACCESS
 #############
 
+SCHEDULE_TYPE_PRACTICE = 1
+SCHEDULE_TYPE_GAME = 2
+SCHEDULE_TYPE_EVENT = 3
+
+
 def insert_test_data():
     with closing(connect_db()) as db:
         db.execute(
             "insert into User (name, password) values ('foo', '123456')")
+        db.execute(
+            """insert into Schedule (type, when_, body) values (
+                 1, '2012-07-29 01:00:00', ?
+               )""", (json.dumps({'loc': u'ほげほげ',
+                                  'court': '5',
+                                  'end': '03:00',
+                                  'no': '123456789',
+                                  'price': '2400',
+                                  'note': ''}), ))
         db.commit()
 
 
@@ -51,6 +66,13 @@ def find_user_by_password(password):
     cur = g.db.execute('SELECT id, name FROM User WHERE password = ?',
                        (password, ))
     return cur.fetchone()
+
+
+def find_schedules(type):
+    cur = g.db.execute("""
+        SELECT * from Schedule
+        WHERE type = ? AND when_ >= datetime('now', 'localtime')""", (type, ))
+    return cur.fetchall()
 
 
 #############
@@ -85,6 +107,30 @@ def do_login(password):
     return True if user else False
 
 
+def make_practice(raw_practice):
+    practice = {}
+    practice.update(raw_practice)
+    body = json.loads(raw_practice['body'])
+    practice.update(body)
+    return practice
+
+
+def make_game(raw_game):
+    game = {}
+    game.update(raw_game)
+    body = json.loads(raw_game['body'])
+    game.update(body)
+    return game
+
+
+def make_event(raw_event):
+    event = {}
+    event.update(raw_event)
+    body = json.loads(raw_event['body'])
+    event.update(body)
+    return event
+
+
 #############
 # VIEWS
 #############
@@ -96,7 +142,11 @@ def index():
 
 @app.route('/schedule')
 def schedule():
-    return redirect(url_for('index'))
+    ps = [make_practice(s) for s in find_schedules(SCHEDULE_TYPE_PRACTICE)]
+    gs = [make_game(s) for s in find_schedules(SCHEDULE_TYPE_GAME)]
+    es = [make_event(s) for s in find_schedules(SCHEDULE_TYPE_EVENT)]
+    return render_template('schedule.html',
+                           practices=ps, games=gs, events=es)
 
 
 @app.route('/member')
