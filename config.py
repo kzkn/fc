652e25ea913ec4b6d59b9dcceb0dc692644480aa
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2012-09-25 00:14:27 +0900

    secret.py のデフォルト指定を修正

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SECRET_FILE = 'secret.py'
+SECRET_FILE = os.path.join(_basedir, 'secret.py')
 DATABASE_URI = os.path.join(_basedir, 'fc.db')
 UPDATE_SCRIPT = os.path.join(_basedir, 'update.sh')
 BBS_PER_PAGE = 20
