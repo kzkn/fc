fc986c5cd3fb2098cb527d04d503142c6f53d11d
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2012-10-30 22:09:56 +0900

    アルバム一覧がなんとなく表示された
    サムネイルをつっつくとアルバム内の画像一覧を出すように
    しようと思ってて、アルバムID込みのURLを指定しようとしてる。
    でもいまんとこは適当。

--- fcsite/static/js/gallery.js
+++ fcsite/static/js/gallery.js
@@ -1,29 +1,53 @@
+var ALBUM_BASE_URL = null;  // ロードする側で設定する
+var USER_ID = '113613698969275302586';
+var THUMBNAIL_SIZE = 160;
+
+var appendThumbnailFor = function(thumbnails) {
+    return function(i, item) {
+        var title = item.media$group.media$title.$t;
+        var thumbnailUrl = item.media$group.media$thumbnail[0].url;
+        var albumId = item.gphoto$id.$t;
+        var href = ALBUM_BASE_URL.replace('@albumid@', albumId);
+
+        /*
+         * Build DOM:
+         *
+         * ul class="thumbnails"
+         *   li class="span2"
+         *     div class="thumbnail"
+         *       a href="..."
+         *         img src="..." title="..." alt="..."
+         *       p class="desc"
+         */
+        thumbnails
+          .append($('<li/>').addClass('span2')
+            .append($('<div/>').addClass('thumbnail')
+              .append($('<a/>').attr('href', href)
+                .append($('<img/>').attr('src', thumbnailUrl)
+                                   .attr('title', title)
+                                   .attr('alt', title)))
+              .append($('<p/>').addClass('desc')
+                               .text(title))));
+    };
+}
+
 var onGetAlbum = function(response) {
-    console.log(response);
-    if (response.feed && response.feed.entry) {
-        var albums = response.feed.entry;
-        for (var i = 0, len = albums.length; i < len; ++i) {
-            console.log('title: ' + albums[i].
-        }
-    }
+    var thumbnails = $('<ul/>').attr('class', 'thumbnails')
+    $('#gallery').append(thumbnails);
+
+    if (response.feed && response.feed.entry)
+        $.each(response.feed.entry, appendThumbnailFor(thumbnails));
 }
 
-var onGetPhoto = function(response) {
-    $('#gallery .body').html('<img src="' + response.feed.entry[0].content.src + '">');
+var insertScript = function(query) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = query;
+    document.body.appendChild(script);
 }
 
-$(function() {
-    function insertScript(query) {
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = query;
-        document.body.appendChild(script);
-    }
-
-    function showAlbumList() {
-      insertScript('https://picasaweb.google.com/data/feed/api/user/113613698969275302586/?kind=album&alt=json-in-script&callback=onGetAlbum&max-results=1');
-      //insertScript('https://picasaweb.google.com/data/feed/api/user/113613698969275302586/albumid/5102862630735366945?alt=json-in-script&callback=onGetAlbum&max-results=1');
-    }
-
-    showAlbumList();
-});
+var showAlbumList = function() {
+    insertScript('http://picasaweb.google.com/data/feed/api/user/' + USER_ID +
+                 '?kind=album&access=visible&alt=json-in-script' +
+                 '&thumbsize=' + THUMBNAIL_SIZE + 'c&callback=onGetAlbum');
+}
