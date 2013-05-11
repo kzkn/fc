1e3c03eed0065a0d1ce1ce6e8f7190c2ce408ce0
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2013-05-11 11:58:07 +0900

    gensecret で ADMIN_EMAIL も出力する

--- gensecret.py
+++ gensecret.py
@@ -1,3 +1,6 @@
 import os
 with open('secret.py', 'w') as f:
-    print >>f, "secrets={'SECRET_KEY':" + repr(os.urandom(24)) + "}"
+    print >>f, "secrets={"
+    print >>f, "  'SECRET_KEY':" + repr(os.urandom(24)) + ","
+    print >>f, "  'ADMIN_EMAIL':'foo@example.com'"
+    print >>f, "}"
